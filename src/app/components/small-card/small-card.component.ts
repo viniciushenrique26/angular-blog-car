8a30b8ec8bba48488730bbd9fd214d58d847ead3
viniciushenrique26 <viniciusslavov@gmail.com>
2023-10-12 12:15:15 -0300

    dynamic blog update

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -10,6 +10,8 @@ export class SmallCardComponent implements OnInit {
   imageCover:string = ""
   @Input()
   cardTitle:string = ""
+  @Input()
+  Id:string = "0"
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/data/fakeData.ts
@@ -0,0 +1,27 @@
+export const fakeData = [
+  {
+    "id" : "1",
+    "title" : "Navigating the Road of Innovation: The Fascinating Evolution of BMW",
+    "description" : "In the world of automotive excellence, BMW has long been a name synonymous with innovation and luxury. Over the decades, this German automaker has forged a path of continuous transformation and adaptation, consistently pushing the boundaries of what's possible in the industry. The story of BMW is more than just a tale of remarkable cars; it's a story of innovation, design, and unwavering commitment to creating the ultimate driving machine. Join us on a journey through time as we explore the captivating evolution of BMW. The roots of BMW trace back to World War I, where the company was established in 1916 as Bayerische Motoren Werke AG. Initially, they manufactured aircraft engines, and their reputation for precision engineering was born. After the war, BMW transitioned to producing motorcycles and eventually entered the automobile market with the iconic Dixi 3/15, a licensed version of the Austin Seven. ",
+    "imageCover" : "https://s2-autoesporte.glbimg.com/fZILpGUAkQZ_wMYO0DXsnknsCVQ=/1200x/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2020/R/e/6sC7VFQkKArBqvl1Nfzg/2015-02-13-bmw-serie-2-16.png",
+
+  },
+  {
+    "id" : "2",
+    "title" : "Charging Ahead: The Future of Electric Cars",
+    "description" : "Electric cars, once considered a futuristic concept, are now a driving force shaping the future of the automotive industry. With increasing environmental concerns, advances in battery technology, and a growing demand for sustainable transportation, electric cars have emerged as a transformative innovation. In this blog post, we'll dive into the electrifying world of electric vehicles (EVs) and explore the promising future they hold. </br> Electric cars are not a new invention. In fact, they predate internal combustion engine vehicles. The early 19th century saw pioneers like Thomas Davenport and Robert Anderson experimenting with electric propulsion. Fast forward to the late 20th century, and the likes of General Motors' EV1 and the Toyota RAV4 EV provided glimpses of what was to come.",
+    "imageCover" : "https://www.byd.com.br/wp-content/uploads/2022/07/BYD-SEAL-BYD-ATTO-3-and-BYD-DOLPHIN-were-showcased-at-the-conference1-scaled.jpg"
+  },
+  {
+    "id" : "3",
+    "title" : "Charging Ahead: The Future of Electric Cars",
+    "description" : "Dive into the future of automotive innovation with our blog, 'Charging Ahead: The Future of Electric Cars.' Explore the latest trends, breakthrough technologies, and the green revolution that is shaping the automotive industry.",
+    "imageCover" : "https://www.byd.com.br/wp-content/uploads/2022/07/BYD-SEAL-BYD-ATTO-3-and-BYD-DOLPHIN-were-showcased-at-the-conference1-scaled.jpg"
+  },
+  {
+    "id" : "4",
+    "title" : "Charging Ahead: The Future of Electric Cars",
+    "description" : "Dive into the future of automotive innovation with our blog, 'Charging Ahead: The Future of Electric Cars.' Explore the latest trends, breakthrough technologies, and the green revolution that is shaping the automotive industry.",
+    "imageCover" : "https://www.byd.com.br/wp-content/uploads/2022/07/BYD-SEAL-BYD-ATTO-3-and-BYD-DOLPHIN-were-showcased-at-the-conference1-scaled.jpg"
+  }
+]
